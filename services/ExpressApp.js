c7b61b1c5566baf1f18b4c6d1cd554618f268c2f
Sairaaj Surve <sairaaj26@gmail.com>
2022-07-11 19:13:53 +0530

    add rate limiter

--- services/ExpressApp.js
+++ services/ExpressApp.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const { UserRouter } = require('../routes/UserRouter')
 const { AdminRouter } = require('../routes/AdminRouter')
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 1, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 
 module.exports.ExpressApp = async (app) => {
 
     app.use(express.json())
     app.use(express.urlencoded({extended: true}))
 
-    app.use('/user', UserRouter)
     app.use('/admin', AdminRouter)
+
+    app.use(limiter)
+    app.use('/user', UserRouter)
 
     app.get('/', async (req, res, next) => {
         return res.status(200).json({message: "Root"})
